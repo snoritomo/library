7b74639e1d93bd5cae59f235206ed12732e6b4f5
suzuki noritomo <suzuki.noritomo@gmail.com>
2014-09-30 23:36:46 +0900

    daterのフォントサイズ見直しとdrop、styless-tabsの追加

---
+++ drop/drop/js/drop.js
@@ -0,0 +1,185 @@
+/**
+	drop
+	auth: noritomo.suzuki@kikisoftware.com
+	条件
+		jqueryがincludeされている事
+	引数
+		labelid: （必須）表示ラベルのid
+		listid: （必須）リストコンテナのid
+		labelclass: (__drop_label)表示ラベルにつけられるclass
+		listclass: (__drop_list)リストコンテナに付けられるclass
+		listitemclass: (__drop_list_item)リストアイテムに付けられるclass
+		triangleurl: (drop/res/triangle.png)右側に表示する画像
+		triangleclass: (__drop_triangle)右側に表示する画像に付けられるclass
+		innertag: (div)自動作成される、選択中の表示をする要素のタグ名
+		innerid: (labelid+'_inner')選択中の表示をする要素のid
+		hiddenid: (labelid+'_hidden')フォーム送信に使用されるinput[hidden]のid
+		settopvaluedefault: (true)表示ラベルの記載がないときに、一番上の要素を初期表示するか
+		duration: (200)ドロップアニメーションの動作時間
+		droponmouseover: (false)マウスオーバーでリストを展開させるか
+		uponmouseleave: (false)マウスリーブでリストを閉じるか
+		labelthemeclass: (__drop_label_theme)テーマ用。選択中の表示をする要素に付けられるclass
+		listitemthemeclass: (__drop_list_item_theme)テーマ用。リストアイテムに付けられるclass
+**/
+function Drop(args){
+	this._labelid = '';
+	this._listid = '';
+	this.labelclass = '__drop_label';
+	this.listclass = '__drop_list';
+	this.listitemclass = '__drop_list_item';
+	this.triangleurl = 'drop/res/triangle.png'
+	this.triangleclass = '__drop_triangle';
+	this.innertag = 'div';
+	this.innerid = '';
+	this.hiddenid = '';
+	this.settopvaluedefault = true;
+	this.duration = 200;
+	this.droponmouseover = false;
+	this.uponmouseleave = false;
+	this.labelthemeclass = '__drop_label_theme';
+	this.listitemthemeclass = '__drop_list_item_theme';
+	if(args!=null){
+		if(args.labelid!=undefined)this._labelid = args.labelid;
+		if(args.listid!=undefined)this._listid = args.listid;
+		if(args.labelclass!=undefined)this.labelclass = args.labelclass;
+		if(args.listclass!=undefined)this.listclass = args.listclass;
+		if(args.listitemclass!=undefined)this.listitemclass = args.listitemclass;
+		if(args.triangleurl!=undefined)this.triangleurl = args.triangleurl;
+		if(args.triangleclass!=undefined)this.triangleclass = args.triangleclass;
+		if(args.innertag!=undefined)this.innertag = args.innertag;
+		if(args.innerid!=undefined)this.innerid = args.innerid;
+		if(args.hiddenid!=undefined)this.hiddenid = args.hiddenid;
+		if(args.settopvaluedefault!=undefined)this.settopvaluedefault = args.settopvaluedefault;
+		if(args.duration!=undefined)this.duration = args.duration;
+		if(args.droponmouseover!=undefined)this.droponmouseover = args.droponmouseover;
+		if(args.uponmouseleave!=undefined)this.uponmouseleave = args.uponmouseleave;
+		if(args.labelthemeclass!=undefined)this.labelthemeclass = args.labelthemeclass;
+		if(args.listitemthemeclass!=undefined)this.listitemthemeclass = args.listitemthemeclass;
+	}
+	if(this.innerid == '')this.innerid = this._labelid+'_inner';
+	if(this.hiddenid == '')this.hiddenid = this._labelid+'_hidden';
+	
+	this.label = $('#'+this._labelid);
+	this.text = this.label.html();
+	this.label.html('');
+	this.label.addClass(this.labelclass);
+	this.list = $('#'+this._listid);
+	this.list.addClass(this.listclass);
+	
+	this.hidden = $(document.createElement('input'));
+	this.hidden.attr('id', this.hiddenid);
+	this.hidden.attr('type', 'hidden');
+	if(this.label.attr('name')!=null){
+		this.hidden.attr('name', this.label.attr('name'));
+		this.label.attr('name', null);
+	}
+	this.label.append(this.hidden);
+	this.inner = $(document.createElement(this.innertag));
+	this.inner.attr('id', this.innerid);
+	this.inner.addClass(this.labelthemeclass);
+	this.label.append(this.inner);
+	
+	this.selectedindex = -1;
+	this.triangle = $(document.createElement('img'));
+	this.triangle.attr('src', this.triangleurl);
+	this.triangle.addClass(this.triangleclass);
+	this.label.append(this.triangle);
+	
+	this.items = [];
+	for(var i = 1; i <= this.list.children().size(); i++){
+		var t = this.list.children(':nth-child('+i+')');
+		t.attr('data-index', i);
+		if(t.attr('value')==undefined)t.attr('value', t.html());
+		t.addClass(this.listitemclass);
+		t.addClass(this.listitemthemeclass);
+		this.items.push(t);
+	}
+	
+	this.maxheight = this.list.height();
+	
+	if(this.text==''){
+		if(this.settopvaluedefault){
+			this.select(0);
+		}
+	}
+	else{
+		for(var i = 1; i <= this.list.children().size(); i++){
+			var t = this.list.children(':nth-child('+i+')');
+			if(t.html()==this.text){
+				this.hidden.val(t.attr('value'));
+				break;
+			}
+		}
+	}
+	
+	this.list.css({
+		height: 0,
+		display: 'none',
+		top: this.label.position().top + this.label.outerHeight(),
+		left: this.label.position().left
+	});
+	this.triangle.on('load', {tgt: this}, function(evt){
+		var t = evt.data.tgt;
+		t.maxwidth = (t.label.innerWidth() > t.list.innerWidth() ? t.label.innerWidth() : t.list.innerWidth()) + t.triangle.width() + parseInt(t.triangle.css('right').replace('px',''));
+		
+		t.label.css({
+			width: t.maxwidth
+		});
+		
+		t.list.css({
+			width: t.maxwidth
+		});
+	});
+	
+	this.downtarget = false;
+	
+	if(this.droponmouseover){
+		this.label.on('mouseenter', {tgt: this}, function(evt){
+			var t = evt.data.tgt;
+			t.down();
+		});
+	}
+	if(this.uponmouseleave){
+		this.list.on('mouseleave', {tgt: this}, function(evt){
+			var t = evt.data.tgt;
+			t.up();
+		});
+	}
+	this.label.on('click', {tgt: this}, function(evt){
+		var t = evt.data.tgt;
+		t.down();
+		t.downtarget = true;
+	});
+	$(document).on('click', {tgt: this}, function(evt){
+		var t = evt.data.tgt;
+		if(t.downtarget){
+			t.downtarget = false;
+			return;
+		}
+		t.up();
+	});
+	this.list.children().on('click', {tgt: this}, function(evt){
+		var t = evt.data.tgt;
+		t.selectItem($(this));
+	});
+};
+Drop.prototype.select = function(idx){
+	var tgt = this.list.children(':nth-child('+(idx+1)+')');
+	this.selectItem(tgt);
+};
+Drop.prototype.selectItem = function(tgt){
+	this.selectedindex = parseInt(tgt.attr('data-index')) + 1;
+	this.inner.html(tgt.html());
+	this.hidden.val(tgt.attr('value'));
+};
+Drop.prototype.down = function(){
+	this.list.css({display: 'inline-block', height: 0});
+	this.list.animate({height: this.maxheight}, {duration: this.duration});
+};
+Drop.prototype.up = function(){
+	var t = this;
+	this.list.animate({height: 0}, {duration: this.duration, complete: function(){t.list.css({display: 'none'});}});
+};
+
+
+
---
+++ stylesstabs/js/styless-tabs.js
@@ -0,0 +1,201 @@
+/**
+	styless-tabs
+	auth: noritomo.suzuki@kikisoftware.com
+	条件
+		jqueryがincludeされている事
+	引数
+		contentcontainerid: （必須）コンテンツ表示コンテナのid
+		indexcontentids: （必須）紐付けるタブインデックスとコンテンツの組み合わせをメンバに持つオブジェクト
+		handlemode: (0)0:全て 1:マウス 2:タッチ
+		defaultzindex: (0)コンテンツコンテナのz-index
+		zindexinterval: (10)前後処理で設定するz-indexの間隔
+		adjustwidth: (true)幅を揃える
+		adjustheight: (true)高さを揃える
+		invisiblehidecontent: (false)最前面以外をdisplay:noneする
+**/
+function StylessTabs(args){
+	this._contentcontainerid = '';
+	this._indexcontentids = {};
+	this.handlemode = 0;
+	this.defaultzindex = 0;
+	this.zindexinterval = 10;
+	this.adjustwidth = true;
+	this.adjustheight = true;
+	this.invisiblehidecontent = false;
+	if(args!=null){
+		if(args.contentcontainerid!=undefined)this._contentcontainerid = args.contentcontainerid;
+		if(args.indexcontentids!=undefined)this._indexcontentids = args.indexcontentids;
+		if(args.handlemode!=undefined)this.handlemode = args.handlemode;
+		if(args.defaultzindex!=undefined)this.defaultzindex = args.defaultzindex;
+		if(args.zindexinterval!=undefined)this.zindexinterval = args.zindexinterval;
+		if(args.adjustwidth!=undefined)this.adjustwidth = args.adjustwidth;
+		if(args.adjustheight!=undefined)this.adjustheight = args.adjustheight;
+		if(args.invisiblehidecontent!=undefined)this.invisiblehidecontent = args.invisiblehidecontent;
+	}
+	this.contentcontainer = $('#'+this._contentcontainerid);
+	this.contentposition = this.contentcontainer.position();
+	this.contenttop = this.contentposition.top;
+	this.contentleft = this.contentposition.left;
+	this.contentwidthdeg = $(window).innerWidth() - this.contentcontainer.width();
+	
+	this.contentwidth = (this.contentcontainer.get(0).style.width!=''?this.contentcontainer.innerWidth():0);
+	this.contentheight = (this.contentcontainer.get(0).style.height!=''?this.contentcontainer.innerHeight():0);
+	this.contentsmaxwidth = 0;
+	this.contentsmaxheight = 0;
+	
+	this.handlemouse = true;
+	this.handletouch = true;
+	if(this.handlemode==1)this.handletouch = false;
+	if(this.handlemode==2)this.handlemouse = false;
+	
+	this.oncontentchange = [];
+	
+	this.tabindexes = [];
+	this.contents = [];
+	
+	var cnt = 0;
+	var digheight = 0;
+	for(var key in this._indexcontentids){
+		var tabindex = $('#'+key);
+		if(tabindex==null || tabindex.size()<=0)continue;
+		var content = $('#'+this._indexcontentids[key]);
+		if(content==null || content.size()<=0)continue;
+		
+		content.css({
+			zIndex: this.defaultzindex - (cnt==0?0:this.zindexinterval),
+			top: 0,
+			left: 0,
+			position: 'absolute'
+		});
+		if(cnt>0 && this.invisiblehidecontent){
+			content.css({
+				display: 'none'
+			});
+		}
+		digheight -= content.outerHeight();
+		if(content.height()>this.contentsmaxheight)this.contentsmaxheight = content.height();
+		if(content.width()>this.contentsmaxwidth)this.contentsmaxwidth = content.width();
+		
+		if(this.handletouch){
+			tabindex.on('touchstart', {tgt: this}, this.change_content);
+		}
+		if(this.handlemouse){
+			tabindex.on('click', {tgt: this}, this.change_content);
+		}
+		
+		this.tabindexes.push(tabindex);
+		this.contents.push(content);
+		cnt++;
+	}
+	this.tabcount = cnt;
+	
+	this.contentcontainer.css({
+		zIndex: this.defaultzindex,
+		position: 'relative'
+	});
+	if(this.adjustheight){
+		this.adjust_height();
+	}
+	if(this.adjustwidth){
+		this.adjust_width();
+	}
+	
+	$(window).on('resize', {tgt: this}, function(evt){
+		var t = evt.data.tgt;
+		t.contentcontainer.width($(window).innerWidth() - t.contentwidthdeg);
+	});
+};
+StylessTabs.prototype.change_content = function(evt){
+	var t = evt.data.tgt;
+	for(var cidx in t.contents){
+		var c = t.contents[cidx];
+		if(c.css==undefined)continue;
+		c.css({
+			zIndex: t.defaultzindex - t.zindexinterval
+		});
+		if(t.invisiblehidecontent){
+			c.css({
+				display: 'none'
+			});
+		}
+	}
+	var idxid = $(this).attr('id');
+	var idx = $('#'+idxid);
+	var cont = $('#'+t._indexcontentids[idxid]);
+	cont.css({
+		zIndex: t.defaultzindex
+	});
+	if(t.invisiblehidecontent){
+		cont.css({
+			display: 'block'
+		});
+	}
+	t.doContentChange(idx, cont);
+};
+StylessTabs.prototype.setOnContentChange = function(f){
+	this.oncontentchange.push(f);
+};
+StylessTabs.prototype.doContentChange = function(tabindex, content){
+	for(var i = 0; i < this.oncontentchange.length; i++){
+		var f = this.oncontentchange[i];
+		f(tabindex, content);
+	}
+};
+StylessTabs.prototype.adjust_height = function(){
+	for(var ckey in this.contents){
+		var cval = this.contents[ckey];
+		if(cval.css==undefined)continue;
+		cval.css({height: this.contentsmaxheight});
+	}
+	this.contentcontainer.css({
+		height: this.contentsmaxheight
+	});
+	this.contentheight = this.contentcontainer.height();
+};
+StylessTabs.prototype.adjust_width = function(){
+	for(var ckey in this.contents){
+		var cval = this.contents[ckey];
+		if(cval.css==undefined)continue;
+		cval.css({width: this.contentsmaxwidth});
+	}
+	this.contentcontainer.css({
+		width: this.contentsmaxwidth
+	});
+	this.contentwidth = this.contentcontainer.width();
+};
+StylessTabs.prototype.adjustHeight = function(h){
+	this.contentsmaxheight = h;
+	this.adjust_height();
+};
+StylessTabs.prototype.adjustWidth = function(w){
+	this.contentsmaxwidth = w;
+	this.adjust_width();
+};
+StylessTabs.prototype.adjustMaxHeight = function(){
+	this.contentsmaxheight = this.getMaxHeight();
+	this.adjust_height();
+};
+StylessTabs.prototype.adjustMaxWidth = function(){
+	this.contentsmaxwidth = this.getMaxWidth();
+	this.adjust_width();
+};
+StylessTabs.prototype.getMaxHeight = function(){
+	var re = 0;
+	for(var ckey in this.contents){
+		var cval = this.contents[ckey];
+		if(cval.css==undefined)continue;
+		if(cval.outerHeight()>re)re = cval.outerHeight();
+	}
+	return re;
+};
+StylessTabs.prototype.getMaxWidth = function(){
+	var re = 0;
+	for(var ckey in this.contents){
+		var cval = this.contents[ckey];
+		if(cval.css==undefined)continue;
+		if(cval.outerWidth()>re)re = cval.outerWidth();
+		cval.css({width: this.contentsmaxwidth});
+	}
+	return re;
+};
+
